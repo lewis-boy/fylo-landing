9c8a606f88e69361d86ff4ee87e9ef72be244b68
Luis Escobar <luisaeniebla@gmail.com>
2023-09-25 21:54:46 -0700

    second push

---
+++ src/components/Testimonial.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { raleway } from '~/pages'
+
+const Testimonial = ({ }) => {
+    return (
+        <div className={`${raleway.variable}` + " font-raleway"}>
+            Testimonial
+        </div>
+    )
+}
+
+export default Testimonial
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,25 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "intro-email-blue": "hsl(217,28%,15%)",
+      "main-blue": "hsl(218,28%,13%)",
+      "footer-blue": "hsl(216,53%,9%)",
+      "testimonial-blue": "hsl(219,30%,18%)",
+      "cyan": "hsl(176,68%,64%)",
+      "robin": "hsl(198,60%,50%)",
+      "red": 'hsl(0, 100%, 63%)',
+      "white": "hsl(0, 0%, 100%)",
+    },
+    screens: {
+      'desktop': '1440px'
+    },
+    extend: {
+      fontFamily: {
+        'opensans': 'var(--font-opensans)',
+        'raleway': 'var(--font-raleway)',
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
